729c817f51452b829f75b55981def98c8448e4c3
asafieva <asafieva@gmail.com>
2023-10-23 11:05:36 -0400

    Test Script v.1.0.

---
+++ MyFirstScript.R
@@ -0,0 +1,12 @@
+#  This is a comment
+# Install gapminder package and 
+instal.packagesl("gapminder")
+library(gapminder)
+install.packages("tidyverse")
+library(tidyverse)
+
+read_data <- gapminder
+dir.create("images")
+dir.create("doc")
+dir.create("figures")
+
